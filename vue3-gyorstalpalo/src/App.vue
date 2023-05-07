5d66cf79ed2711e0415812f97d07c14b407dfb7a
HorvLala <horvathwebdev@gmail.com>
2023-05-07 22:01:38 +0200

    first commit

---
+++ vue3-gyorstalpalo/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import Welcome from './components/Welcome.vue'</script>
+<template>
+  <header></header>
+  <main>
+    <Welcome />
+  </main>
+  <footer></footer>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300&family=Rubik:wght@400;500;600;700&display=swap");
+:root {
+  --blue: #576ca8;
+  --inputBg: #444b67;
+  --cr1: #c94277;
+  --cr2: #1b998b;
+  --cr3: #ffd046;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #302b27;
+  color: #fff;
+}
+#app {
+  font-family: Quicksand, sans-serif;
+}
+
+main {
+  padding: 0.5rem;
+}
+
+input {
+  border: none;
+  border-radius: 0.5rem;
+  background-color: var(--inputBg);
+  padding: 0.5rem;
+}
+
+.df {
+  display: flex;
+  gap: 1rem;
+}
+</style>
---
+++ vue3-gyorstalpalo/src/components/Welcome.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>taskR</h1>
+    <div class="df">
+      <h2>A leg<strong>cool</strong>abb task kezelő evör</h2>
+      <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+      />
+    </div>
+    <form class="df">
+      <input type="email" placeholder="e-mail">
+      <input type="password" placeholder="jelszó">
+      <button>Login</button>
+    </form>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 7rem;
+  text-align: center;
+  font-weight: 600;
+}
+h2 {
+  font-size: 1.7rem;
+  text-align: right;
+  font-weight: normal;
+}
+img {
+  width: 100px;
+  height: 100px;
+}
+form{
+  background-color: var(--blue);
+  flex-direction: column;
+  border-radius: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  margin-top: 2rem;
+}
+
+input {
+  font-size: 1.5rem;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: var(--inputBg);
+  padding: 0.5rem;
+}
+button {
+  background-color: var(--blue);
+  color: #fff;
+  border:none;
+  font-size: 1.5rem;
+}
+</style>
+
+34:20
